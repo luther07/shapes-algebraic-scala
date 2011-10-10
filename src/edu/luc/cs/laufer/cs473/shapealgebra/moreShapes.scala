b2c3930b33b7669be68c72657506bdc4b1809fc2
Mark Johnson <luther07@gmail.com>
2011-10-10 17:24:43 -0500

    defined missing additional shapes, without having them inherit from eachother

--- src/edu/luc/cs/laufer/cs473/shapealgebra/moreShapes.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/moreShapes.scala
@@ -6,4 +6,18 @@ import java.awt.Color
  * Some additional shape classes added later.
  */
 
+case class Stroke(c: Color, s: Shape) extends Decorator
+
+case class Fill(s: Shape) extends Decorator
+
+case class Outline(s: Shape) extends Decorator
+
+case class Point(x: Int, y: Int) extends Shape
+
+case class Polygon(ps: Point*) extends Shape
+
+case Rotate(deg: Int, s: Shape) extends Decorator
+
+case Circle(r: Int) extends Shape
+
 // TODO: your job (avoid inheritance among case classes)
