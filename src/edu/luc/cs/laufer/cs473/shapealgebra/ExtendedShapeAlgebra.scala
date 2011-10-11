0e64b2cbed8d5e5758fb915d4d3a0b3a822328c3
Mark Johnson <luther07@gmail.com>
2011-10-10 20:21:05 -0500

    change to shape of visitStroke

--- src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
@@ -5,14 +5,27 @@ package edu.luc.cs.laufer.cs473.shapealgebra
  */
 trait ExtendedShapeAlgebra[R] extends ShapeAlgebra[R] {
 
-  def visitPolygon(p: Polygon): R
+  def visitStroke(r: R, s: Stroke): R
+  def visitFill(s: Shape): R
+  def visitOutline(s: Shape): R
+  def visitPoint(p: Point): R
+  def visitPolygon(ps: Seq[R], p:Polygon): R
+  def visitRotate(r: Rotate): R
+  def visitCircle(c: Circle): R
+
   // TODO: add missing visit methods similarly to Location
 
   /**
    * The extended catamorphism for shapes.
    */
   override def fold(s: Shape): R = s match {
-    case p: Polygon => visitPolygon(p)
+    case s: Stroke => visitStroke(fold(s.shape), s)
+    case f: Fill => visitFill(f)
+    case o: Outline => visitOutline(o)
+    case p: Point => visitPoint(p)
+    case p: Polygon => visitPolygon(p.points.map(fold(_)), p)
+    case r: Rotate => visitRotate(r)
+    case c: Circle => visitCircle(c)
     // TODO: add missing cases similarly to Location
     case _ => super.fold(s)
   }
