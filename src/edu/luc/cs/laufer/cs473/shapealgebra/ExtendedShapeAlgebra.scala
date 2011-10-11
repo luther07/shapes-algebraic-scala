94b4898ece3e191c7696083338edb4492912e779
Mark Johnson <luther07@gmail.com>
2011-10-10 20:23:42 -0500

    change the shape of visitStroke, to visit children

--- src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
@@ -7,7 +7,7 @@ trait ExtendedShapeAlgebra[R] extends ShapeAlgebra[R] {
 
   def visitStroke(r: R, s: Stroke): R
   def visitFill(r: R, s: Shape): R
-  def visitOutline(s: Shape): R
+  def visitOutline(r: R, s: Shape): R
   def visitPoint(p: Point): R
   def visitPolygon(ps: Seq[R], p:Polygon): R
   def visitRotate(r: Rotate): R
@@ -21,7 +21,7 @@ trait ExtendedShapeAlgebra[R] extends ShapeAlgebra[R] {
   override def fold(s: Shape): R = s match {
     case s: Stroke => visitStroke(fold(s.shape), s)
     case f: Fill => visitFill(fold(f.shape), f)
-    case o: Outline => visitOutline(o)
+    case o: Outline => visitOutline(fold(o.shape), o)
     case p: Point => visitPoint(p)
     case p: Polygon => visitPolygon(p.points.map(fold(_)), p)
     case r: Rotate => visitRotate(r)
