fad8b04be0809847a3d2ce42615a7e767ceea50a
Mark Johnson <luther07@gmail.com>
2011-11-06 07:00:58 -0600

    formatting changes

--- project/build/sbt.scala
+++ project/build/sbt.scala
@@ -1,12 +1,16 @@
 import sbt._
 class ShapesAlgebraicScala(info: ProjectInfo) extends DefaultProject(info) {
-  val junit = "junit" % "junit" % "4.6"
   
-  override def mainScalaSourcePath = "src" / "edu" / "luc" / "cs" / "laufer" / "cs473" / "shapealgebra"
+   val junit = "junit" % "junit" % "4.6"
+
+   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.9.1"
   
-  override def mainResourcesPath = "resources"
+   override def mainScalaSourcePath = "src" / "edu" / "luc" / "cs" / "laufer" / "cs473" / "shapealgebra"
   
-  override def testScalaSourcePath = "test" / "edu" / "luc" / "cs" / "laufer" / "cs473" / "shapealgebra"
+   override def mainResourcesPath = "resources"
   
-  override def testResourcesPath = "test-resources"
+   override def testScalaSourcePath = "test" / "edu" / "luc" / "cs" / "laufer" / "cs473" / "shapealgebra"
+  
+   override def testResourcesPath = "test-resources"
+  
 }
